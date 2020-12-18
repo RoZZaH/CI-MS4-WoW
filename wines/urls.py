e7e460c0244407020e80b9f8501409ea96782678
rossgeo <rossg@live.ie>
2020-12-18 14:08:45 +0000

    Wine Listings - CBV for Type, Search, Specials

--- wines/urls.py
+++ wines/urls.py
@@ -9,8 +9,16 @@ urlpatterns = [
         name='list'
     ),
     path(
+        route='specials/',
+        view=views.WineSpecials.as_view(),
+        name='specials'),
+    path(
+        route='type/<str:wtype>/',
+        view=views.WineTypeView.as_view(),
+        name='type'),
+    path(
         route='<slug:slug>/',
         view=views.WineDetailView.as_view(),
         name='detail'
-    )
+    ),
 ]
--- wines/views.py
+++ wines/views.py
@@ -1,12 +1,44 @@
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Wine
 
 class WineListView(ListView):
+    
     template_name = "wines_list.html"
     model = Wine
+    paginate_by = 5
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        q = self.request.GET.get("q")
+        if q:
+            return qs.filter(Q(name__icontains=q) | Q(description__icontains=q))
+        return qs
+
+
+class WineTypeView(ListView):
+
+    template_name = "wines_list.html"
+    model = Wine
+    paginate_by = 5
+    
+    def get_queryset(self, *args, **kwargs):
+        return Wine.objects.filter(wtype__icontains=self.kwargs.get('wtype'))
 
 
 class WineDetailView(DetailView):
+
     template_name = "wines_detail.html"
     model = Wine
+
+
+class WineSpecials(ListView):
+    
+    template_name = "wines_list.html"
+    model = Wine
+    paginate_by = 5
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(discounted_price__isnull=False)
