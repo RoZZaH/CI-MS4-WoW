e2aa7580140a37faf7f2db6bd665e5fb77722d00
rossgeo <rossg@live.ie>
2020-12-22 19:58:56 +0000

    Tidied up Checkout Form - Delivery Charges Computation Outstanding

--- checkout/forms.py
+++ checkout/forms.py
@@ -30,7 +30,7 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'country':
+            if field != 'country': #adds placeholder to all but country field
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *' #if req
                 else:
