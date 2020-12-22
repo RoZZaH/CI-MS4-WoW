037d2726b62df745354b141a7fb65c4dacde18ee
rossgeo <rossg@live.ie>
2020-12-22 21:48:50 +0000

    Registration Working and Customer Profile wired to User

--- customers/models.py
+++ customers/models.py
@@ -26,7 +26,7 @@ class Customer(models.Model):
                                    null=True, blank=True)
 
     def __str__(self):
-        return self.customer.username
+        return self.user.username
 
 
 @receiver(post_save, sender=User)
@@ -37,4 +37,4 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Customer.objects.create(user=instance)
     # Existing users: just save the profile
-    instance.userprofile.save()
+    instance.customer.save()
--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,13 @@
-# from django.shortcuts import render
 from django.views import View
 from django.views.generic import DetailView, TemplateView
-
+from .models import Customer
 
 class CustomerProfile(TemplateView):
     template_name = "customers_profile.html"
+    model = Customer
+
+    def get_context_data(self, **kwargs):
+        profile = self.model.objects.get(user=self.request.user)
+        context = super().get_context_data(**kwargs)
+        context["profile"] = profile
+        return context
